a91c673956ef660b45417025d46bc44ed7bee397
4ukaTuJlka <134392340+4ukaTuJlka@users.noreply.github.com>
2023-07-14 18:09:20 +0300

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,18 @@
+const arrow = (x, n) => {
+    let result = x;
+
+    for (let i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+    alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+    alert(arrow(x, n));
+}
